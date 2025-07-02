17b4cd10749f6291e316676505fb6a26b5c40ea6
Pham Tri Trong <tritrongpham3105@gmail.com>
2025-07-02 13:39:09 +0700

    Code

--- src/Bai1.java
+++ src/Bai1.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Bai1 {
+    public static int Sum(int n){
+        if (n == 0){
+            return 0;
+        }
+        return n + Sum(n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Sum(n));
+    }
 }
